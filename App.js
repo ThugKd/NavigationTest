38e5fb5fb1378783e11505a92140c5b26f6292e1
unknown <qrwang@i9i8.com>
2019-05-06 17:05:26 +0800

    tabNavigation

--- App.js
+++ App.js
@@ -6,39 +6,98 @@
  * @flow
  */
 
-import React from "react";
-import {Text, View} from "react-native";
-import {createAppContainer, createStackNavigator} from "react-navigation";
+import React from 'react';
+import {Text, View} from 'react-native';
+import {createBottomTabNavigator, createAppContainer} from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 class HomeScreen extends React.Component {
     render() {
         return (
-            <View
-                style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-                <Text>Home Screen</Text>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Home!</Text>
             </View>
         );
     }
 }
 
-class DetailsScreen extends React.Component {
+class SettingsScreen extends React.Component {
     render() {
         return (
-            <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-                <Text>Details Screen</Text>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Settings!</Text>
             </View>
         );
     }
 }
 
-
-const AppNavigator = createStackNavigator(
-    {
-        Home: HomeScreen,
-        Details: DetailsScreen
-    },
-    {
-        initialRouteName: "Details"
+class IconWithBadge extends React.Component {
+    render() {
+        const { name, badgeCount, color, size } = this.props;
+        return (
+            <View >
+                <Ionicons name={name} size={size} color={color} />
+                {badgeCount > 0 && (
+                    <View
+                        style={{
+                            // /If you're using react-native < 0.57 overflow outside of the parent
+                            // will not work on Android, see https://git.io/fhLJ8
+                            position: 'absolute',
+                            right: 0,
+                            top: 2,
+                            backgroundColor: 'red',
+                            borderRadius: 6,
+                            width: 12,
+                            height: 12,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}>
+                        <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
+                            {badgeCount}
+                        </Text>
+                    </View>
+                )}
+            </View>
+        );
     }
+}
+
+const HomeIconWithBadge = props => {
+    // You should pass down the badgeCount in some other ways like context, redux, mobx or event emitters.
+    return <IconWithBadge {...props} badgeCount={3} />;
+};
+
+const getTabBarIcon = (navigation, focused, tintColor) => {
+    const { routeName } = navigation.state;
+    let IconComponent = Ionicons;
+    let iconName;
+    if (routeName === 'Home') {
+        iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+        // We want to add badges to home tab icon
+        IconComponent = HomeIconWithBadge;
+    } else if (routeName === 'Settings') {
+        iconName = `ios-options${focused ? '' : '-outline'}`;
+    }
+
+    // You can return any component that you like here!
+    return <IconComponent name={iconName} size={25} color={tintColor} />;
+};
+
+export default createAppContainer(
+    createBottomTabNavigator(
+        {
+            Home: { screen: HomeScreen },
+            Settings: { screen: SettingsScreen },
+        },
+        {
+            defaultNavigationOptions: ({ navigation }) => ({
+                tabBarIcon: ({ focused, tintColor }) =>
+                    getTabBarIcon(navigation, focused, tintColor),
+            }),
+            tabBarOptions: {
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+            },
+        }
+    )
 );
-export default createAppContainer(AppNavigator);
